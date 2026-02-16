4caf9fabf9d70d9adcdbcb6352df187354be5fac
mariale07 <maryalemontoya@gmail.com>
2026-02-15 20:31:11 -0600

    Ejercicio 2
    Realicé el ejercicio No.2

--- L4_MAMP_1155326/L4_MAMP_1155326/Program.cs
+++ L4_MAMP_1155326/L4_MAMP_1155326/Program.cs
@@ -8,7 +8,22 @@ class Program {
         int nivel = 15;
         float puntos_de_vida = 75.8F;
         bool es_un_jefe = false;
+        //Salida de datos
         Console.WriteLine(nombre + " " + nivel + " " + puntos_de_vida + " " + es_un_jefe);
+        Console.ReadLine();
+
+        //EJERCICIO 2 - Conversión impícita (de pequeño a grande)
+        //Declaración de variables
+        int numeroEntero = 1500;
+        long numeroLargo;
+        numeroLargo = numeroEntero;
+        //Salida de datos
+        Console.WriteLine(numeroLargo);
+        Console.ReadLine();
+        //Declaro una nueva variable
+        double numeroDecimal = numeroLargo;
+        //Salida de datos
+        Console.WriteLine("El número decimal es: " + numeroDecimal);
         Console.ReadLine();
     }
 }
