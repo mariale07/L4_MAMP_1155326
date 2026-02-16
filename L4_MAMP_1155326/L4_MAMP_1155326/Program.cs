cb132a61e8202b1e5e9db388d7d3b327a022cb5e
mariale07 <maryalemontoya@gmail.com>
2026-02-15 20:24:12 -0600

    Ejercicio 1
    Realice el ejercicio No. 1

---
+++ L4_MAMP_1155326/L4_MAMP_1155326/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program {
+    static void Main() {
+
+        //EJERCICIO 1 - El perfil de un personaje
+        //Declaración de variables
+        string nombre = "Elena";
+        int nivel = 15;
+        float puntos_de_vida = 75.8F;
+        bool es_un_jefe = false;
+        Console.WriteLine(nombre + " " + nivel + " " + puntos_de_vida + " " + es_un_jefe);
+        Console.ReadLine();
+    }
+}
