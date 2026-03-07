a95e9ec98218cdb6d9865a3c8bdf15a121e02be1
mariale07 <maryalemontoya@gmail.com>
2026-03-06 23:41:08 -0600

    Ejercicio 1
    realicé el ejercicio 1

--- L7_MAMP_1155326/L7_MAMP_1155326/Program.cs
+++ L7_MAMP_1155326/L7_MAMP_1155326/Program.cs
@@ -3,4 +3,31 @@ Console.WriteLine("¿Cómo te llamas?");
 string nombre = Console.ReadLine();
 //Salida de datos
 Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+Console.ReadLine();
 
+Console.WriteLine("EJERCICIO 1 - WHILE");
+Console.WriteLine("-------------------");
+//Mostrar los números del 1 al 20.
+string nom = "Maria Alejandra Montoya Pineda";
+int carnet = 1155326;
+int indice = 1;
+
+Console.WriteLine("Nombre: " + nom + "Carnet: " + carnet.ToString());
+
+while (indice <= 20)
+{
+
+    if (indice % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+    Console.WriteLine(indice);
+    indice = indice + 1;
+}
+Console.ForegroundColor = ConsoleColor.White;
+Console.ReadLine();
