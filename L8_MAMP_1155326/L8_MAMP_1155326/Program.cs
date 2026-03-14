9bd2b81c42eaad7be8529897fa45f20d6f9271c5
mariale07 <maryalemontoya@gmail.com>
2026-03-14 16:03:27 -0600

    Ejercicio 2
    Realicé el ejercicio No.2

--- L8_MAMP_1155326/L8_MAMP_1155326/Program.cs
+++ L8_MAMP_1155326/L8_MAMP_1155326/Program.cs
@@ -33,3 +33,28 @@ Console.WriteLine("El número mayor es: " + mayor);
 Console.WriteLine("El número menor es: " + menor);
 Console.WriteLine("El promedio es: " + promedio);
 Console.ReadLine();
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Ejercicio 2");
+Console.WriteLine("--------------------------------");
+for (int j=1; j<=100; j++)
+{
+    if(j % 2==0 && j % 7 == 0)
+    {
+        Console.WriteLine(j + " = ParSiete");
+    }
+    else if (j % 2 == 0)
+    {
+    Console.WriteLine(j + " = Par");
+    } else if(j % 7 == 0){
+    Console.WriteLine(j + " = Siete");
+    }
+    else
+    {
+    Console.WriteLine(j + " = No es múltiplo de 2 ni de 7.");
+    }
+}
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Ejercicio 3");
+Console.WriteLine("--------------------------------");
