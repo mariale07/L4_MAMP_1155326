8d5dceff6c6481e3d87c0dd9c40252b99bc66cbb
mariale07 <maryalemontoya@gmail.com>
2026-03-20 19:58:17 -0600

    Ejercicio 1
    Realicé el ejercicio No.1

--- L9_MAMP_1155326/L9_MAMP_1155326/Program.cs
+++ L9_MAMP_1155326/L9_MAMP_1155326/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Entrada de usuario
+        Console.Write("¿Cómo te llamas? ");
+        string nombre = Console.ReadLine();
+        // Salida de datos
 
+        Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+
+        //Ejercicio 1
+        Console.WriteLine("----------EJERCICIO 1-----------");
+        caracter("Raton");
+        caracter("Insomnio");
+    }
+    //Ejercicio 1 - procedimiento
+    static void caracter(string a)
+    {
+        Console.WriteLine(a.Length);
+    }
+}
