117d976aed470179a4b4be439ae5d6dbcab67041
mariale07 <maryalemontoya@gmail.com>
2026-03-20 21:33:40 -0600

    Ejercicio 4
    Realicé el ejercicio No.4

--- L9_MAMP_1155326/L9_MAMP_1155326/Program.cs
+++ L9_MAMP_1155326/L9_MAMP_1155326/Program.cs
@@ -30,6 +30,28 @@ class Program
         Console.WriteLine("A= " + a);
         Console.WriteLine("B= " + b);
 
+        //Ejercicio 3
+        double precio = 48.50;
+        double descuento = 0.15;
+
+        Console.WriteLine("----------EJERCICIO 3-----------");
+
+        //Antes
+        Console.WriteLine("Precio original: " + precio);
+
+        aplicardes(descuento, ref precio);
+
+        Console.WriteLine("Precio con descuento: " + precio);
+
+        //Ejercicio 4
+        int puntosSalud = 10;
+        Console.WriteLine("----------EJERCICIO 4-----------");
+        mostrarSalud(puntosSalud);
+        recibirDaño(ref puntosSalud);
+        mostrarSalud(puntosSalud);
+        curar(ref puntosSalud);
+        mostrarSalud(puntosSalud);
+        calificarDesempeno(puntosSalud);
     }
     //Ejercicio 1 - procedimiento
     static void caracter(string a)
@@ -44,5 +66,70 @@ class Program
         x = y;
         y = temp;
     }
-
+    //Ejercicio 3 - procedimiento
+    static void aplicardes(double descuento, ref double precio)
+    {
+        precio = precio - (precio * descuento);
+    }
+    //Ejercicio 4 - procedimiento
+    //a) Recibir daño
+    static void recibirDaño(ref int puntosSalud)
+    {
+        puntosSalud = puntosSalud - 5;
+        if(puntosSalud<0)
+        {
+            puntosSalud = 0;
+        }
+    }
+    //b) curar
+    static void curar(ref int puntosSalud)
+    {
+        puntosSalud = puntosSalud + 3;
+        if(puntosSalud>15)
+        {
+            puntosSalud = 15;
+        }
+    }
+    //c) mostrad salud
+    static void mostrarSalud(int puntosSalud)
+    {
+        if (puntosSalud >= 11)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else if(puntosSalud>=6)
+        { 
+            Console.ForegroundColor=ConsoleColor.Yellow;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        Console.WriteLine("Salud actual: " + puntosSalud);
+        Console.ResetColor();
+    }
+    // d) calificar desempeño
+    static void calificarDesempeno(int puntosSalud)
+    {
+        if (puntosSalud == 15)
+        {
+            Console.WriteLine("Calificación: S");
+        }
+        else if (puntosSalud >= 11)
+        {
+            Console.WriteLine("Calificación: A");
+        }
+        else if (puntosSalud >= 6)
+        {
+            Console.WriteLine("Calificación: B");
+        }
+        else if (puntosSalud >= 1)
+        {
+            Console.WriteLine("Calificación: C");
+        }
+        else
+        {
+            Console.WriteLine("Calificación: Derrotado");
+        }
+    }
 }
