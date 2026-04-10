983cdc6b6ec3534c49d530591033471002f4d988
mariale07 <maryalemontoya@gmail.com>
2026-04-10 17:48:29 -0600

    Ejercicio 1
    Realicé el ejercicio No.1

--- L10_MAMP_1155326/L10_MAMP_1155326/Program.cs
+++ L10_MAMP_1155326/L10_MAMP_1155326/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Entrada de usuario
+        Console.Write("¿Cómo te llamas? ");
+        string nombre = Console.ReadLine();
+        // Salida de datos
+        Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
 
+        //EJERCICIO 1
+        Console.WriteLine("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int resultado = sumardigitos(numero);
+
+        Console.WriteLine("La suma de los dígitos es: " + resultado);
+    }
+    //Función
+    static int sumardigitos(int numero)
+    { 
+    int suma = 0;
+
+        while (numero > 0)
+        {
+            suma = suma + (numero %10);
+            numero = numero / 10;
+        }
+return suma;
+    }
+
+}
