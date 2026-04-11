68cc524105ee935086596f040f402151b97aacd6
mariale07 <maryalemontoya@gmail.com>
2026-04-10 20:16:57 -0600

    Ejercicio 2
    Realicé el ejercicio 2

--- L10_MAMP_1155326/L10_MAMP_1155326/Program.cs
+++ L10_MAMP_1155326/L10_MAMP_1155326/Program.cs
@@ -16,18 +16,40 @@ class Program
         int resultado = sumardigitos(numero);
 
         Console.WriteLine("La suma de los dígitos es: " + resultado);
+
+        //EJERCICIO 2
+        Console.WriteLine("Ingrese su primer nombre");
+        String nombre1 = Console.ReadLine();
+        Console.WriteLine("Ingrse su segundo nombre: ");
+        string nombre2 = Console.ReadLine();
+        Console.WriteLine("Ingrese su apellido paterno: ");
+        string apellido1 = Console.ReadLine();
+        Console.WriteLine("Ingrese su apellido materno: ");
+        string apellido2 = Console.ReadLine();
+
+        string correo = generar_correo(nombre1, nombre2, apellido1, apellido2);
+
+        Console.WriteLine("Correo institucional: " + correo);
     }
-    //Función
+    //Función - ejercicio 1
     static int sumardigitos(int numero)
-    { 
-    int suma = 0;
+    {
+        int suma = 0;
 
         while (numero > 0)
         {
-            suma = suma + (numero %10);
+            suma = suma + (numero % 10);
             numero = numero / 10;
         }
-return suma;
+        return suma;
+    }
+    //Función - ejercicio 2
+    static string generar_correo(string nombre1, string nombre2, string apellido1, string apellido2)
+    {
+    string correo = "";
+    correo = nombre1.Substring(0,1) + nombre2.Substring(0,1) + apellido1 + apellido2.Substring(0,1) + "@correo.url.edu.gt";
+
+        return correo.ToLower();
     }
 
 }
