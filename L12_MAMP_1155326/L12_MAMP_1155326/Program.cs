d28da050a205a85a8d82f414b8011205950436c0
mariale07 <maryalemontoya@gmail.com>
2026-04-24 19:21:20 -0600

    Ejercicio 1
    Realicé el ejercicio 1

--- L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
+++ L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
@@ -1,3 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Entrada de usuario
+            //EJERCICIO 1
+        Console.WriteLine("----------Ejercicio 1----------");
+        int[,] numeros = new int[5, 5];
 
+        //Llamar procedimiento
+        Llenar(numeros);
+
+        //LLamar funciones
+        int sumaPrincipal = diagonalPrincipal(numeros);
+        int sumaSecundaria = diagonalSecundaria(numeros);
+
+        Console.WriteLine("Suma de la diagonal principal: " + sumaPrincipal);
+        Console.WriteLine("Suma de la diagonal secundaria: " + sumaSecundaria);
+        
+        // Salida de datos
+       
+    }
+
+    //EJERCICIO 1
+
+    static void Llenar(int[,] a)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j=0; j<5; j++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                a[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+    static int diagonalPrincipal(int[,] a)
+    {
+        int suma = 0;
+        {
+            for(int i=0; i<5; i++)
+            {
+                suma = suma + a[i,i];
+            }
+
+            return suma;
+        }
+    }
+
+    static int diagonalSecundaria(int[,] a)
+    {
+        int suma = 0;
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                suma = suma + a[i, 4-i];
+            }
+
+            return suma;
+        }
+    }
+}
