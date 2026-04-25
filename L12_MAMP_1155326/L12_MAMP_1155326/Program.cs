d5a6403ffcb27f0221d4b925fdeeac530fe9b658
mariale07 <maryalemontoya@gmail.com>
2026-04-24 19:40:31 -0600

    Ejercicio 2
    Realicé el ejercicio 2

--- L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
+++ L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
@@ -3,9 +3,9 @@ class Program
 {
     static void Main()
     {
-        // Entrada de usuario
             //EJERCICIO 1
         Console.WriteLine("----------Ejercicio 1----------");
+        Console.WriteLine();
         int[,] numeros = new int[5, 5];
 
         //Llamar procedimiento
@@ -17,9 +17,21 @@ class Program
 
         Console.WriteLine("Suma de la diagonal principal: " + sumaPrincipal);
         Console.WriteLine("Suma de la diagonal secundaria: " + sumaSecundaria);
-        
-        // Salida de datos
-       
+
+        //EJERCICIO 2
+        Console.WriteLine();
+        Console.WriteLine("----------Ejercicio 2----------");
+        Console.WriteLine();
+        int[,] enteros = new int[4, 6];
+
+        llenar(enteros);
+
+        int pares = numerosPares(enteros);
+        int impares = numerosImpares(enteros);
+
+        Console.WriteLine("Hay " + pares + " numeros pares.");
+        Console.WriteLine("Hay " + impares + " numeros impares.");
+
     }
 
     //EJERCICIO 1
@@ -60,5 +72,51 @@ class Program
 
             return suma;
         }
+    }
+
+
+    //Ejercicio 2
+    static void llenar(int[,] b)
+    {
+        for (int i=0; i<4; i++)
+        {
+            for (int j=0; j<6; j++)
+            {
+                Console.WriteLine("Ingrese un número: ");
+                b[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+    static int numerosPares(int[,] b)
+    {
+        int contador = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (b[i, j] % 2 == 0)
+                    {
+                        contador = contador + 1;
+                    }
+                }
+            }
+        return contador;
+    }
+
+    static int numerosImpares(int[,] b)
+    {
+        int contador = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                if (b[i, j] % 2 != 0)
+                {
+                    contador = contador + 1;
+                }
+            }
+        }
+        return contador;
     }
 }
