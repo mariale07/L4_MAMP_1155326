021d0098f1f7ab289c6e123875416ea7f58cdf4f
mariale07 <maryalemontoya@gmail.com>
2026-04-24 21:19:36 -0600

    Ejercicio 3
    Realicé el ejercicio 3

--- L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
+++ L12_MAMP_1155326/L12_MAMP_1155326/Program.cs
@@ -18,10 +18,12 @@ class Program
         Console.WriteLine("Suma de la diagonal principal: " + sumaPrincipal);
         Console.WriteLine("Suma de la diagonal secundaria: " + sumaSecundaria);
 
+
         //EJERCICIO 2
         Console.WriteLine();
         Console.WriteLine("----------Ejercicio 2----------");
         Console.WriteLine();
+
         int[,] enteros = new int[4, 6];
 
         llenar(enteros);
@@ -32,6 +34,33 @@ class Program
         Console.WriteLine("Hay " + pares + " numeros pares.");
         Console.WriteLine("Hay " + impares + " numeros impares.");
 
+
+        //EJERCICIO 3
+        Console.WriteLine();
+        Console.WriteLine("----------Ejercicio 3----------");
+        Console.WriteLine();
+        float[,] notas = new float[5, 4];
+
+        llenarNotas(notas);
+
+        for (int i=0; i<5; i++)
+        {
+            float promedio = promedioEstudiante(notas, i);
+            bool estado = estadoEstudiante(promedio);
+
+            if (estado == true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El estudiante aprobó." + (i+1));
+            }
+            else if (estado == false)
+            {
+                Console.WriteLine("El estudiante reprobó");
+            }
+            Console.WriteLine("El promedio del estudiante es: " + promedio);
+        }
+
+        
     }
 
     //EJERCICIO 1
@@ -75,7 +104,7 @@ class Program
     }
 
 
-    //Ejercicio 2
+    //EJERCICIO 2
     static void llenar(int[,] b)
     {
         for (int i=0; i<4; i++)
@@ -118,5 +147,42 @@ class Program
             }
         }
         return contador;
+    }
+
+
+    //EJERCICIO 3
+    static void llenarNotas(float[,] c)
+    {
+        for (int i=0; i<5; i++)
+        {
+            Console.WriteLine("Estudiante " + (i + 1));
+            for (int j = 0; j<4; j++)
+            {
+                Console.WriteLine("Ingrese la nota del estudiante " + (j + 1) + " : ");
+                c[i, j] = float.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+    }
+
+    static float promedioEstudiante(float [,] c, int estudiante)
+    {
+        float suma = 0;
+            for(int j=0; j < 4; j++)
+            {
+                suma = suma + c[estudiante, j];
+            }
+        return suma / 4;
+    }
+
+    static bool estadoEstudiante(float promedio)
+    {
+        if (promedio >= 61)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 }
